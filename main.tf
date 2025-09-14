32272c2b029a23e3d7d8be219362b24bf8007246
Gabgrz <gab.120gg@gmail.com>
2025-09-14 03:15:29 -0600

    add module im-workspace

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ module "service_accounts" {
     "gke-showroom=>roles/compute.admin"
   ]
 }
+
+module "im-workspace" {
+ source = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
+ version = "~> 7.0"
+
+ project_id = "gke-showroom"
+ deployment_id = "create-sa"
+ im_deployment_repo_uri = "https://github.com/gabrielgaroz/terraform-google-service-accounts.git"
+ im_deployment_ref = "REF"
+
+ github_app_installation_id = "85843775"
+}
