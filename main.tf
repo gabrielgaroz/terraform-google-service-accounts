f93759231e490233610a229db891db551dccfd84
Gabgrz <gab.120gg@gmail.com>
2025-09-14 01:52:42 -0600

    add initial SA

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "service_accounts" {
+  source        = "terraform-google-modules/service-accounts/google"
+  version       = "~> 4.0"
+  project_id    = "gke-showroom"
+  prefix        = "dev"
+  names         = ["awesome-sa"]
+  display_name  = "Awesome SA"
+  description   = "An awesome SA"
+  project_roles = [
+    "roles/compute.imageUser",
+    "roles/compute.networkUser"
+  ]
+}
