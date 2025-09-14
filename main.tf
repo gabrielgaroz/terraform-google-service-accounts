77366b4dbfbc6fefee46e04aa5c5c4286e2e381f
Gabgrz <gab.120gg@gmail.com>
2025-09-14 02:19:55 -0600

    update SA

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ module "service_accounts" {
   version       = "~> 4.0"
   project_id    = "gke-showroom"
   prefix        = "dev"
-  names         = ["cute-sa"]
-  display_name  = "Cute SA"
-  description   = "A cute SA"
+  names         = ["my-sa"]
+  display_name  = "My SA"
+  description   = "My SA"
   project_roles = [
     "gke-showroom=>roles/container.admin",
     "gke-showroom=>roles/compute.admin"
